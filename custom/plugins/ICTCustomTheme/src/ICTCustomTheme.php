3433e1c1639759d9ac5a584dd98d29735993713f
divyesh1484 <divyesh.solanki@icreativetechnolabs.com>
2024-05-13 11:39:12 +0530

    Custom theme Create.

---
+++ custom/plugins/ICTCustomTheme/src/ICTCustomTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace ICTCustomTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class ICTCustomTheme extends Plugin implements ThemeInterface
+{
+}
